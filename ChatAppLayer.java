3d7edca1e747f7fb1552c4e16c436f789cfe7f3a
woody35545 <woody35545@gmail.com>
2022-09-30 17:08:41 +0900

    Override getter/setter

--- ChatAppLayer.java
+++ ChatAppLayer.java
@@ -60,32 +60,41 @@ public class ChatAppLayer implements BaseLayer{
 	@Override
 	public String GetLayerName() {
 		// TODO Auto-generated method stub
-		return null;
+		return pLayerName;
 	}
 
 	@Override
 	public BaseLayer GetUnderLayer() {
-		return null;
+		if (p_UnderLayer == null)
+			return null;
+		return p_UnderLayer;
 	}
 
 	@Override
 	public BaseLayer GetUpperLayer(int nindex) {
-		return null;
+		if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+			return null;
+		return p_aUpperLayer.get(nindex);
 	}
 
 	@Override
 	public void SetUnderLayer(BaseLayer pUnderLayer) {
-
+		if (pUnderLayer == null)
+			return;
+		this.p_UnderLayer = pUnderLayer;
 	}
 
 	@Override
 	public void SetUpperLayer(BaseLayer pUpperLayer) {
-	
+		if (pUpperLayer == null)
+			return;
+		this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);
 	}
 
 	@Override
 	public void SetUpperUnderLayer(BaseLayer pUULayer) {
-			
+		this.SetUpperLayer(pUULayer);
+		pUULayer.SetUnderLayer(this);
 	}
 
 }
