b682898d39064764e4bd09519d2fda87ac585788
woody35545 <woody35545@gmail.com>
2022-10-01 02:08:04 +0900

    Add compare address function

--- Utils.java
+++ Utils.java
@@ -1,7 +1,19 @@
-
 public final class Utils {
-/*
- * This class implements useful functions that can be used in general for implementing layer logic. 
- */
+	/*
+	 * This class implements useful functions that can be used in general for
+	 * implementing layer logic.
+	 */
+	public static boolean compare_address(byte[] address_1, byte[] address_2) {
+		/*
+		 * Address comparison function defined in byte array format. 
+		 * Available for both mac and ip addresses
+		 */
+		for (int i = 0; i < address_1.length; i++) {
+			if (((byte) address_1[i]) != ((byte) address_2[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
